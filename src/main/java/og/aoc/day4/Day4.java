d01130273131d61683bcf276fd1727e974347623
xLillium <nicolas.motillon@gmail.com>
2023-12-07 00:36:32 +0100

    day4: part 2

--- src/main/java/og/aoc/day4/Day4.java
+++ src/main/java/og/aoc/day4/Day4.java
@@ -4,13 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day4 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/day4/input.txt";
         List<String> scratchCards = Files.readAllLines(Paths.get(filePath));
         part1(scratchCards);
+        part2(scratchCards);
+    }
+
+    private static void part2(List<String> scratchCards) {
+        Map<Integer, Integer> cardCopies = new HashMap<>();
+        int totalCards = scratchCards.size();
+
+        for (int i = 0; i < totalCards; i++) {
+            cardCopies.put(i + 1, 1);
+        }
+
+        for (int i = 0; i < totalCards; i++) {
+            int currentCardNumber = i + 1;
+            int amountOfCopiesOfCurrentCard = cardCopies.get(currentCardNumber);
+
+            for (int copy = 0; copy < amountOfCopiesOfCurrentCard; copy++) {
+                int amountOfWinningNumbers = (int) getAmountOfWinningNumbers(scratchCards.get(i));
+                for (int j = 1; j <= amountOfWinningNumbers && (currentCardNumber + j) <= totalCards; j++) {
+                    cardCopies.merge(currentCardNumber + j, 1, Integer::sum);
+                }
+            }
+        }
+
+        int sum = cardCopies.values().stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Total scratchcards: " + sum);
     }
 
     private static void part1(List<String> scratchCards) {
